3157c3702359f3c73e6a8364b8a9ecc9d6a69c72
shinsuke <shinsuke.mat@gmail.com>
2017-03-01 16:12:00 +0900

    egitのデフォルトのチェックアウト先ロケーションを変更 C:\users\%USERNAME%\gitから，C:\pbl\workspaceに

--- setup-conf.sh
+++ setup-conf.sh
@@ -119,6 +119,7 @@ retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.prefs
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.workbench.prefs
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.wst.xml.ui.prefs
+retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
 cd $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/
 echo -e 'eclipse.preferences.version=1\nencoding=utf-8\nversion=1' > org.eclipse.core.resources.prefs
@@ -128,6 +129,8 @@ echo -e 'eclipse.preferences.version=1\nshowIntro=false' > org.eclipse.ui.prefs
 echo -e 'eclipse.preferences.version=1\nPLUGINS_NOT_ACTIVATED_ON_STARTUP=com.xored.glance.ui;de.loskutov.anyedit.AnyEditTools;junit.extensions.eclipse.quick;junit.extensions.eclipse.quick.mock;net.sf.eclipsecs.ui;org.eclipse.buildship.stsmigration;org.eclipse.cft.server.verify.ui;org.eclipse.epp.logging.aeri.ide;org.eclipse.epp.mpc.ui;org.eclipse.equinox.p2.ui.sdk.scheduler;org.eclipse.jst.j2ee.webservice.ui;org.eclipse.m2e.discovery;org.eclipse.mylyn.builds.ui;org.eclipse.mylyn.tasks.ui;org.eclipse.mylyn.team.ui;org.eclipse.rse.ui;org.eclipse.swtbot.eclipse.ui;org.eclipse.swtbot.generator;org.eclipse.team.svn.ui.startup;org.eclipse.ui.monitoring;org.sf.feeling.decompiler;' > org.eclipse.ui.workbench.prefs
 echo -e 'REMOTE_COMMANDS_VIEW_FONT=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\nfindBugsEclipsePlugin.consoleFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.compare.contentmergeviewer.TextMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.debug.ui.DetailPaneFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.debug.ui.MemoryViewTableFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.debug.ui.consoleFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.egit.ui.CommitMessageEditorFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.egit.ui.CommitMessageFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.egit.ui.DiffHeadlineFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.internal.ui.compare.JavaMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.internal.ui.compare.PropertiesFileMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.ui.PropertiesFileEditor.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.ui.editors.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jface.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.mylyn.wikitext.ui.presentation.textFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.pde.internal.ui.compare.ManifestContentMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.pde.internal.ui.compare.PluginContentMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.recommenders.snipmatch.rcp.searchTextFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.wst.jsdt.internal.ui.compare.JavaMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.wst.jsdt.ui.editors.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.wst.sse.ui.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\npreference.console.font=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\nterminal.views.view.font.definition=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;' >> org.eclipse.ui.workbench.prefs
 echo -e 'eclipse.preferences.version=1\norg.eclipse.wst.xml.ui.internal.tabletree.XMLMultiPageEditorPart.lastActivePage=1' > org.eclipse.wst.xml.ui.prefs
+_PATH=$(echo $PBL_HOME'/workspace' | xargs cygpath -w | sed 's|\\|\\\\\\|g' | sed 's|:|\\:|')
+echo -e 'core_defaultRepositoryDir='$_PATH > org.eclipse.egit.core.prefs
 wget http://sdl.ist.osaka-u.ac.jp/~shinsuke/pbl.zip/misc/workbench.xmi -O $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
 cd $PBL_HOME
 
@@ -198,6 +201,9 @@ cleanup_eclipse() {
 cleanup_chrome() {
   rm -rf chrome/Data/profile
 }
+cleanup_workspace() {
+  find $PBL_HOME/workspace -maxdepth 1 -mindepth 1 | grep -v .metadata | xargs rm -rf
+}
 cleanup() {
   cleanup_mongodb
   cleanup_tomcat
@@ -234,5 +240,5 @@ extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.s
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.prefs
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.workbench.prefs
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.wst.xml.ui.prefs
+extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
-
