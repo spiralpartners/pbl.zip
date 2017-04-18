0375e95d0c384e941f0470738780044ccf158608
shinsuke <shinsuke.mat@gmail.com>
2017-04-18 09:27:59 +0900

    fix url

--- setup-hdp.sh
+++ setup-hdp.sh
@@ -1,8 +1,6 @@
 ########################################
 # init
 
-export VERSION="ver 1.4"
-
 export TMP=/d/tmp/
 export PBL_HOME=/c/pbl/
 
@@ -11,7 +9,7 @@ mkdir -p $PBL_HOME
 
 ########################################
 cd $TMP
-wget http://sdl.ist.osaka-u.ac.jp/~shinsuke/pbl.zip/hadoop-2.7.3.tar.gz
-tar zxvf hadoop-2.7.3.tar.gz
+wget http://sdl.ist.osaka-u.ac.jp/~shinsuke/pbl.zip/hadoop-2.7.3-win32.tar.gz
+tar zxvf hadoop-2.7.3-win32.tar.gz
 rm -rf $PBL_HOME/hadoop
-mv hadoop-2.7.3 $PBL_HOME/hadoop
+mv hadoop-2.7.3-win32 $PBL_HOME/hadoop
