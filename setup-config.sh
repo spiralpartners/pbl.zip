eb66c87945f46e6646dd21a5cb4d44078015e664
shinsuke-mat <shinsuke-mat@users.noreply.github.com>
2017-04-27 19:05:18 +0900

    Gradleのローカルリポジトリの生成場所を変更 > C:\pbl\workspace\.gradle

--- setup-config.sh
+++ setup-config.sh
@@ -14,7 +14,7 @@
 ########################################
 # init
 
-export VERSION="ver 1.4"
+export VERSION="ver 1.5"
 
 export TMP=/d/tmp/
 export PBL_HOME=/c/pbl/
@@ -117,6 +117,7 @@ echo -e 'MAX_RECENT_WORKSPACES=10\nRECENT_WORKSPACES='$_PATH'\nRECENT_WORKSPACES
 # - パースペクティブレイアウトの全体情報
 # - xmlを開く際のデフォルトエディタを設計ではなくソースに
 # - egitのデフォルトパスを$USER_HOME$からworkspaceフォルダに
+# - gradleホームを固定
 cd $PBL_HOME
 rm -rf workspace
 mkdir -p workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/
@@ -128,6 +129,7 @@ retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.workbench.prefs
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.wst.xml.ui.prefs
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs
+retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.buildship.core.prefs
 retain_restore $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
 cd $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/
 echo -e 'eclipse.preferences.version=1\nencoding=utf-8\nversion=1' > org.eclipse.core.resources.prefs
@@ -137,6 +139,8 @@ echo -e 'eclipse.preferences.version=1\nshowIntro=false' > org.eclipse.ui.prefs
 echo -e 'eclipse.preferences.version=1\nPLUGINS_NOT_ACTIVATED_ON_STARTUP=com.xored.glance.ui;de.loskutov.anyedit.AnyEditTools;junit.extensions.eclipse.quick;junit.extensions.eclipse.quick.mock;net.sf.eclipsecs.ui;org.eclipse.buildship.stsmigration;org.eclipse.cft.server.verify.ui;org.eclipse.epp.logging.aeri.ide;org.eclipse.epp.mpc.ui;org.eclipse.equinox.p2.ui.sdk.scheduler;org.eclipse.jst.j2ee.webservice.ui;org.eclipse.m2e.discovery;org.eclipse.mylyn.builds.ui;org.eclipse.mylyn.tasks.ui;org.eclipse.mylyn.team.ui;org.eclipse.rse.ui;org.eclipse.swtbot.eclipse.ui;org.eclipse.swtbot.generator;org.eclipse.team.svn.ui.startup;org.eclipse.ui.monitoring;org.sf.feeling.decompiler;' > org.eclipse.ui.workbench.prefs
 echo -e 'REMOTE_COMMANDS_VIEW_FONT=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\nfindBugsEclipsePlugin.consoleFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.compare.contentmergeviewer.TextMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.debug.ui.DetailPaneFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.debug.ui.MemoryViewTableFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.debug.ui.consoleFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.egit.ui.CommitMessageEditorFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.egit.ui.CommitMessageFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.egit.ui.DiffHeadlineFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.internal.ui.compare.JavaMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.internal.ui.compare.PropertiesFileMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.ui.PropertiesFileEditor.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jdt.ui.editors.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.jface.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.mylyn.wikitext.ui.presentation.textFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.pde.internal.ui.compare.ManifestContentMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.pde.internal.ui.compare.PluginContentMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.recommenders.snipmatch.rcp.searchTextFont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.wst.jsdt.internal.ui.compare.JavaMergeViewer=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.wst.jsdt.ui.editors.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\norg.eclipse.wst.sse.ui.textfont=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\npreference.console.font=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;\nterminal.views.view.font.definition=1|Consolas|11.25|0|WINDOWS|1|-15|0|0|0|400|0|0|0|0|3|2|1|49|Consolas;' >> org.eclipse.ui.workbench.prefs
 echo -e 'eclipse.preferences.version=1\norg.eclipse.wst.xml.ui.internal.tabletree.XMLMultiPageEditorPart.lastActivePage=1' > org.eclipse.wst.xml.ui.prefs
+_PATH=$(cygpath -w $PBL_HOME | sed 's|\\|/|g')
+echo -e 'gradle.user.home='$_PATH'workspace/.gradle' > org.eclipse.buildship.core.prefs
 _PATH=$(echo $PBL_HOME'workspace' | xargs cygpath -w | sed 's|\\|\\\\\\|g' | sed 's|:|\\:|')
 echo -e 'core_defaultRepositoryDir='$_PATH > org.eclipse.egit.core.prefs
 wget http://sdl.ist.osaka-u.ac.jp/~shinsuke/pbl.zip/workbench.xmi -O $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
@@ -258,4 +262,5 @@ extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.s
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.ui.workbench.prefs
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.wst.xml.ui.prefs
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.egit.core.prefs
+extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.buildship.core.prefs
 extract_delta $PBL_HOME/workspace/.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
